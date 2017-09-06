763a75f9bfdd029110b60ac9937b340484edeb89
Bartosz Sądel <bartosz.sadel@onwelo.com>
2017-09-06 08:50:16 +0200

    Refactor

--- lib/message_handler.ex
+++ lib/message_handler.ex
@@ -1,7 +1,7 @@
 defmodule MessageHandler do
   
   def handle_msg({:error, reason}, socket) do
-    IO.puts 'Error while receiving message from socker. Error reason: #{reason}'
+    IO.puts 'Error while receiving message from socket. Error reason: #{reason}'
     :gen_tcp.close(socket)
     # handle error and close kill process
   end
@@ -12,7 +12,7 @@ defmodule MessageHandler do
     IO.puts 'Parsing HTTP 1.1 GET request...'
     IO.puts 'Path: #{abs_path}'
     
-    {path, params} = abs_path |> to_string |> PathParser.parse_path()
+    {path, params} = abs_path |> to_string |> PathParser.parse_path
     IO.puts "Request path params:"
     IO.inspect params
     IO.puts "Splited path:"
@@ -22,16 +22,16 @@ defmodule MessageHandler do
     IO.inspect headers
 
     # send response
+    send_response(socket)
   end
 
-  
   #Handler for HTTP 1.1 POST method.
   #This method receives request and parses it.
   def handle_msg({:ok, {:http_request, :POST, {:abs_path, abs_path}, {1,1}}}, socket) do
     IO.puts 'Parsing HTTP 1.1 POST request...'
     IO.puts 'Path: #{abs_path}'
     
-    {path, params} = abs_path |> to_string |> PathParser.parse_path()
+    {path, params} = abs_path |> to_string |> PathParser.parse_path
     IO.puts "Request path params:"
     IO.inspect params
     IO.puts "Splited path:"
@@ -46,4 +46,29 @@ defmodule MessageHandler do
            |> BodyParser.get_body(socket)
   end
 
+  defp send_response(client_socket) do
+    http_version = 'HTTP/1.1'
+    http_status = '200 OK\r\n'
+    content = "<html><body>Hello World!</body></html>"
+    headers = String.length(content)
+              |> get_base_headers_as_string()
+    response = '#{http_version} #{http_status}#{headers}\r\n#{content}\r\n'
+    IO.puts response
+    :inet.setopts(client_socket, [packet: :http])
+    res = {:http_respone, {1,1}, response}
+    client_socket
+    |> :gen_tcp.send(response)
+  end
+
+  defp current_date() do
+    {:ok, date} = Timex.now("Europe/Warsaw")
+                  |> Timex.format("{RFC1123}")
+    date
+  end
+
+  defp get_base_headers_as_string(content_length) do
+    date = current_date()
+    ['Date: #{date}\r\n', 'Server: SimpleElixirHttpServer/0.0.1\r\n', 'Last-Modified: #{date}\r\n', 'Content-Length: #{content_length}\r\n', 'Connection: close\r\n', 'Content-type: text/html;\r\n', 'Cache-Control: no-cache\r\n']
+    |> Enum.join("")
+  end
 end
--- lib/simple_elixir_http_server.ex
+++ lib/simple_elixir_http_server.ex
@@ -25,41 +25,7 @@ defmodule HTTPServer do
 
   defp work(client_socket) do
     client_socket
-    |> read_msg()
-
-    send_response(client_socket)
-    #work(client_socket)
-  end
-
-  defp read_msg(client_socket) do
-    :gen_tcp.recv(client_socket, 0)
+    |> :gen_tcp.recv(0)
     |> MessageHandler.handle_msg(client_socket)
-  end
-
-  defp send_response(client_socket) do
-    http_version = 'HTTP/1.1'
-    http_status = '200 OK\r\n'
-    server = 'Server: SimpleElixirHttpServer/0.0.1\r\n'
-    connection_header = 'Connection: close\r\n'
-    content_type_header = 'Content-type: text/html;\r\n'
-    charset_header = 'charset=UTF-8\r\n'
-    cache_header = 'Cache-Control: no-cache\r\n'
-    date = current_date()
-    content = "<html><body>Hello World!</body></html>"
-    c_length = String.length(content)
-    content_length = 'Content-Length: #{c_length}\r\n'
-    response = '#{http_version} #{http_status}Date: #{date}\r\n#{server}Last-Modified: #{date}\r\n#{content_length}#{connection_header}#{content_type_header}#{cache_header}\r\n#{content}\r\n'
-    IO.puts response
-    :inet.setopts(client_socket, [packet: :http])
-    res = {:http_respone, {1,1}, response}
-    client_socket
-    |> :gen_tcp.send(response)
-    #|> :gen_tcp.send([http_version, '200', "\r\n", [date, connection_header, content_type_header, charset_header, cache_header], "\r\n", <<>>])
-  end
-
-  defp current_date() do
-    {:ok, date} = Timex.now("Europe/Warsaw")
-                  |> Timex.format("{RFC1123}")
-    date
   end
 end
