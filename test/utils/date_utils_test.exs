ca26185ac1c47c11d747bff755ee1de5eb3c0ccd
Bartosz Sądel <bartosz.sadel@onwelo.com>
2017-09-24 22:17:45 +0200

    Missing test for wrong date format

--- test/utils/date_utils_test.exs
+++ test/utils/date_utils_test.exs
@@ -1,4 +1,9 @@
 defmodule DateUtilsTest do
   use ExUnit.Case
   doctest DateUtils
+
+  test "should return current date for wrong date" do
+    dt = DateUtils.parse_date("wrong_date")
+    assert Timex.now |> Timex.shift(minutes: -1) |> Timex.before?(dt)
+  end
 end
